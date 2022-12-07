9c758399032d339117a99144d2018ec64dbfef01
sukruaydin <sukraydin29@gmail.com>
2022-12-07 20:16:11 +0300

    changes added

---
+++ src/test/java/finspire/spartan_auth/ConfigDemoTest.java
@@ -0,0 +1,20 @@
+package finspire.spartan_auth;
+
+import org.junit.jupiter.api.Test;
+import utilities.ConfigReader;
+
+public class ConfigDemoTest {
+
+    @Test
+    public void test1(){
+
+        String property1 = ConfigReader.getProperty("serenity.project.name");
+        String property2 = ConfigReader.getProperty("spartan.editor.username");
+
+        System.out.println("property1 = " + property1);
+        System.out.println("property2 = " + property2);
+
+    }
+
+
+}
--- src/test/java/finspire/spartan_auth/editor/SpartanAuthApi_PostTest.java
+++ src/test/java/finspire/spartan_auth/editor/SpartanAuthApi_PostTest.java
@@ -1,5 +1,6 @@
 package finspire.spartan_auth.editor;
 
+import io.cucumber.java.it.Ma;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import net.serenitybdd.junit5.SerenityTest;
@@ -7,9 +8,13 @@ import net.serenitybdd.rest.Ensure;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.yecht.Data;
 import utilities.SpartanAuthTestBase;
 import utilities.SpartanUtils;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static net.serenitybdd.rest.SerenityRest.*;
@@ -53,14 +58,40 @@ public class SpartanAuthApi_PostTest extends SpartanAuthTestBase {
 
     }
 
-    @DisplayName("parameterization")
-    @Test
-    public void test2(){
+
+    @ParameterizedTest(name = "New Spartan {index} - name: {0}")
+    @CsvFileSource(resources = "/SpartanData.csv",numLinesToSkip = 1)
+    public void test2(String name, String gender, long phone){
          /*
             we can give name to each execution using name = ""
             and if you want to get index of iteration we can use {index}
             and also if you are to include parameter in your test name
             {0},{1},{2} --> based on the order you provide as argument.
          */
+
+        System.out.println("name = " + name);
+        System.out.println("gender = " + gender);
+        System.out.println("phone = " + phone);
+
+        Map<String,Object> mapSpartan = new LinkedHashMap<>();
+        mapSpartan.put("name",name);
+        mapSpartan.put("gender",gender);
+        mapSpartan.put("phone",phone);
+
+        given().and().auth().basic("editor","editor")
+                .and().contentType(ContentType.JSON)
+                .and().body(mapSpartan)
+                .when().post("/spartans");
+
+        Ensure.that("StatusCode is 201",c -> c.statusCode(201));
+        Ensure.that("Content-type is Json",c -> c.contentType(ContentType.JSON));
+        Ensure.that("Success message is A Spartan is Born!",c -> c.body("success", Matchers.is("A Spartan is Born!")));
+        Ensure.that("id is not null",c -> c.body("data.id",Matchers.notNullValue()));
+        Ensure.that("name is correct",c -> c.body("data.name",Matchers.is(mapSpartan.get("name"))));
+        Ensure.that("gender is correct",c -> c.body("data.gender",Matchers.is(mapSpartan.get("gender"))));
+        Ensure.that("phone is correct",c -> c.body("data.phone",Matchers.is(mapSpartan.get("phone"))));
+        Ensure.that("check location header ends with newly generated id",
+                c -> c.header("Location",Matchers.endsWith(lastResponse().jsonPath().getString("data.id"))));
+
     }
 }
